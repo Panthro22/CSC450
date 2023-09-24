6f56ac60417049865de8950c6254627fcac2881f
Panthro22 <jordonpaynter22@gmail.com>
2023-09-24 16:35:27 -0600

    Refactor to reduce redundancy

--- CTA2/stringConcate.cpp
+++ CTA2/stringConcate.cpp
@@ -6,11 +6,36 @@ using namespace std;
 
 bool isSafeInput(const string &input)
 {
-  // Regular expression to check for safe input (letters, numbers, and spaces)
+  // Regular expression to check for safe input to eliminate special characters
   regex pattern("^[A-Za-z0-9 ]+$");
   return regex_match(input, pattern);
 }
+string getValidInput()
+{
+  string userInput;
 
+  while (true)
+  {
+    getline(cin, userInput);
+
+    // Validate the input
+    if (userInput.length() > 255)
+    {
+      cout << "Invalid amount of Characters. Please enter up to 255 characters." << endl;
+      cout << "Enter the string: "; // Reprompt for input
+    }
+    else if (!isSafeInput(userInput))
+    {
+      cout << "Invalid Characters. Please enter only alphanumeric characters." << endl;
+      cout << "Enter the string: "; // Reprompt for input
+    }
+    else
+    {
+      break; // Input is valid, exit the loop Since loop is infinitely true, we need to break out of it
+    }
+  }
+  return userInput;
+}
 int main()
 {
   for (int i = 0; i < 3; i++)
@@ -19,30 +44,12 @@ int main()
     cout << "Iteration " << i + 1 << endl;
 
     // Prompt the user to enter the first string
-    cout << "Enter the first string (up to 255 characters): ";
-    string firstString;
-    getline(cin, firstString);
-
-    // Validate the first input
-    while (!isSafeInput(firstString) || firstString.length() > 255)
-    {
-      !isSafeInput(firstString) ? cout << "Invalid input. Please enter only alphanumeric characters (up to 255 characters):" << endl : cout << "Invalid input. Please enter only alphanumeric characters (up to 255 characters):" << endl;
-      cout << "Enter the first string: "; // Reprompt for input
-      getline(cin, firstString);
-    }
+    cout << "Enter the first string up to 255 characters: ";
+    string firstString = getValidInput();
 
     // Prompt the user to enter the second string
-    cout << "Enter the second string (up to 255 characters): ";
-    string secondString;
-    getline(cin, secondString);
-
-    // Validate the second input
-    while (!isSafeInput(secondString) || secondString.length() > 255)
-    {
-      !isSafeInput(secondString) ? cout << "Invalid input. Please enter only alphanumeric characters up to 255 characters: " << endl : cout << "Invalid input. Please enter only alphanumeric characters up to 255 characters: " << endl;
-      cout << "Enter the second string: "; // Reprompt for input
-      getline(cin, secondString);
-    }
+    cout << "Enter the second string up to 255 characters: ";
+    string secondString = getValidInput();
 
     // Concatenate the two strings
     string concatenatedString = firstString + secondString;
@@ -51,5 +58,5 @@ int main()
     cout << "Final String: " << concatenatedString << endl;
   }
 
-  return 0;
+  return 0; // End of main needed for C++ programs to compile successfully otherwise you get a linker error
 }
